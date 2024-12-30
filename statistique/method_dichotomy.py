9080cd4344e131cc7b8808ac058a30d8d5d9e84f
yousseftarhri <yousseftarhri@gmail.com>
2024-12-30 19:22:12 +0100

    newton & dichtomy

---
+++ statistique/method_dichotomy.py
@@ -0,0 +1,21 @@
+def dichotomy(f, a, b, epsilon):
+    """
+    Implements the dichotomy (bisection) method for finding the root of a function.
+
+    Parameters:
+    f (function): The function for which we are finding the root.
+    a (float): The starting point of the interval.
+    b (float): The ending point of the interval.
+    epsilon (float): The tolerance for convergence; the algorithm stops when the interval width is less than epsilon.
+
+    Returns:
+    float: The estimated root of the function.
+    """
+    while b - a > epsilon:
+        m = (a + b) / 2
+        if f(a) * f(b) > 0:
+            a = m
+        else:
+            b = m
+
+    return (a + b) / 2
